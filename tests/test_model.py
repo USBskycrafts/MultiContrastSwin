18060dba2705b57914bf060f348e5870cc8c7981
Junran Young <Junran Young>
2025-02-19 00:34:06 +0800

    Add multi-contrast attention mechanism test

--- tests/test_model.py
+++ tests/test_model.py
@@ -12,3 +12,12 @@ class TestTransformer(unittest.TestCase):
 
         y = transformer(x, selected_contrats)
         self.assertEqual(y.shape, (32, 2, 128, 128, 1))
+
+    def test_transformer2(self):
+        transformer = MultiContrastSwinTransformer(
+            dim=8, num_layers=4, window_size=(4, 4), shift_size=(2, 2), num_contrats=8, num_heads=4)
+        x = torch.randn(32, 5, 128, 128, 1)
+        selected_contrats = [[0, 3, 4, 7, 5], [1, 2, 6]]
+
+        y = transformer(x, selected_contrats)
+        self.assertEqual(y.shape, (32, 3, 128, 128, 1))
